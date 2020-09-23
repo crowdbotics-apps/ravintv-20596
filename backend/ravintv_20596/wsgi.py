95188a4401c03f45b12a7233b09cd236df2cc8dc
Crowdbotics <team@crowdbotics.com>
2020-09-23 04:31:25 +0000

    Update models

--- backend/ravintv_20596/wsgi.py
+++ backend/ravintv_20596/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ravintv_20596.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ravintv_20596.settings")
 
 application = get_wsgi_application()
